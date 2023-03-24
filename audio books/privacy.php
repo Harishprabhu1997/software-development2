9599b71685d730681fda9cca01d473c9febd5555
Harish Prabhu <harishprabhu1997@gmail.com>
2023-03-24 17:51:25 +0000

    privacy implementation by abraham

---
+++ audio books/privacy.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privacy Policy</title>
+</head>
+<body>
+    <h1>Privacy Policy</h1>
+    <p>This website is a fictional website created for educational purposes. It does not collect any personal information.</p>
+    <p>For more information, please contact the website administrator at <a href="mailto:example@gmail.com"> or call 555-555-5555.</a></p>
+</body>
+</html>
