0fa2471c8b83d83aefea41609848860df33167d9
Harish Prabhu <harishprabhu1997@gmail.com>
2023-03-24 17:46:26 +0000

    terms implemantation done by abraham

---
+++ audio books/terms.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms of Service</title>
+</head>
+<body>
+    <h1>Terms of Service</h1>
+    <p>This website is a fictional website created for educational purposes. It does not collect any personal information.</p>
+    <p>For more information, please contact the website administrator at <a href="mailto:example@gmail.com"> or call 555-555-5555.</a></p>
+</body>
+</html>
