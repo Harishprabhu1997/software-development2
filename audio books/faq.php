438770eab301c5382afab06a2745529924a30ebd
Harish Prabhu <harishprabhu1997@gmail.com>
2023-03-24 17:56:01 +0000

    faq implementation done by mahesh

---
+++ audio books/faq.php
@@ -0,0 +1,34 @@
+<!-- faq page -->
+<DOCTYPE html>
+<html>
+<head>
+    <title>FAQ - Audio Book Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h2>FAQ</h2>
+    </div>
+    <div class="content">
+        <p>Here are some frequently asked questions about our website.</p>
+        <p>1. How do I find the audio books I want?</p>
+        <!-- Answer -->
+        <p>Use the search bar to find the audio books you want.</p>
+        <p>2. How do I purchase an audio book?</p>
+        <!-- Answer -->
+        <p>Click on the audio book you want to purchase.</p>
+        <p>3. How do I download an audio book?</p>
+        <!-- Answer -->
+        <p>Click on the audio book you want to download.</p>
+        <p>4. How do I listen to an audio book?</p>
+        <!-- Answer -->
+        <p>Click on the audio book you want to listen to.</p>
+        <p>5. How do I return an audio book?</p>
+        <!-- Answer -->
+        <p>Click on the audio book you want to return.</p>
+        <p>6. How do I cancel my account?</p>
+        <!-- Answer -->
+        <p>Click on the account you want to cancel.</p>
+    </div>
+</body>
+</html>
